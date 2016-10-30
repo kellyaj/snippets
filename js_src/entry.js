4df04f81410a19e4c1c1f7316ab5517c1351cf03
Andrew Kelly <kellyaj@gmail.com>
2016-10-30 14:30:29 -0700

    old src rendering, no styles

--- js_src/entry.js
+++ js_src/entry.js
@@ -1,5 +1,1 @@
-var _ = require('lodash');
-
-_.times(5, function(i) {
-  console.log(i);
-});
+require('./index');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = module.exports = {
   context: __dirname,
   entry: './js_src/entry.js',
+  plugins: [
+    new ExtractTextPlugin('static/css/[name].[contenthash:8].css')
+  ]
 };
 
 config.output = {
@@ -16,5 +20,25 @@ config.output = {
 
 config.resolve = {
   extensions: ['', '.js'],
+  alias: {
+    'babel-runtime/regenerator': require.resolve('babel-runtime/regenerator')
+  },
   modulesDirectories: [ 'node_modules' ]
 };
+
+config.module = {
+  loaders: [
+    {
+      test: /\.js$/,
+      include: path.resolve(__dirname, './js_src'),
+      loader: 'babel',
+      query: require('./js_config/babel.dev')
+    },
+    {
+      test: /\.css$/,
+      include: path.resolve(__dirname, ''),
+      loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss')
+    }
+  ]
+}
+
